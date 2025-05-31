6b835ba3af855d995495389a369ee6c9e5f0df2c
Kijima Daigo <norimaking777@gmail.com>
2025-06-01 00:31:29 +0900

    ci: production build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/bview/" : "./",
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? "/bview/" : '/',
   plugins: [react()],
   server: {
     port: 8008,
   },
-});
+}));
