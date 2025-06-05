6d041214a5f6b9e3351056f7c81385f6c75dfdab
Kijima Daigo <norimaking777@gmail.com>
2025-06-06 01:02:18 +0900

    feat: lessを追加した

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.less";
 import { FileLoadPage } from "./pages/FileLoadPage";
 import { ViewPage } from "./pages/ViewPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
