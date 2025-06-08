03f50f77b82c3f8c7020792ad31b79c9a06129c8
Kijima Daigo <norimaking777@gmail.com>
2025-06-08 12:32:23 +0900

    chore: リンクをバッジにした

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import { Box, Flex, HStack, Heading } from "@chakra-ui/react";
+import { Badge, Box, Flex, HStack, Heading } from "@chakra-ui/react";
 import { InternalLink } from "../components/InternalLink";
 
 export const AppLayout = () => {
@@ -12,16 +12,31 @@ export const AppLayout = () => {
               <InternalLink to="/">BVIEW</InternalLink>
             </Heading>
             <HStack>
-              <InternalLink to="/view?f=sample/ls">ls</InternalLink>
-              <InternalLink to="/view?f=sample/rfc9593.txt">txt</InternalLink>
-              <InternalLink to="/view?f=sample/go_hello">
-                go binary
-              </InternalLink>
-              <InternalLink to="/view?f=sample/tree.jpg">JPG</InternalLink>
-              <InternalLink to="/view?f=sample/tree_gray.jpg">
-                JPG(gray)
-              </InternalLink>
-              <InternalLink to="/view?f=sample/cat.png">PNG</InternalLink>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/ls">ls</InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/rfc9593.txt">txt</InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/go_hello">
+                  Go binary
+                </InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/a.out">C binary</InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/tree.jpg">JPG</InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/tree_gray.jpg">
+                  JPG(gray)
+                </InternalLink>
+              </Badge>
+              <Badge variant="outline">
+                <InternalLink to="/view?f=sample/cat.png">PNG</InternalLink>
+              </Badge>
             </HStack>
           </HStack>
         </Flex>
