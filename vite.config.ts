47b9de4fed75e01ec054b6f964352d108b856de0
Kijima Daigo <norimaking777@gmail.com>
2025-06-14 22:23:14 +0900

    refactor: リポジトリ名を定数化した

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repositoryName = "bview";
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bview/" : "/",
+  base: mode === "production" ? `/${repositoryName}/` : "/",
   plugins: [react()],
   server: {
     port: 8008,
